7102616f69ddbbe84a9c10b35bc30dbcd1dd3bbc
Raxz-834 <rrahadiawan@gmail.com>
2024-10-16 08:35:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function showMessage() {
+    const message = document.getElementById('message');
+    message.textContent = "Aku sayang kamu, dan aku akan selalu mencintaimu selamanya, oh iya ayangnya jangan ngeselin yaa,jangan ngomong kasar terus :( nda boleh galak galak ke akunya yaa,dan semangat kerjanyaaaa love you sayangnya aku Firanti Anjaninya aku ❤️";
+}
